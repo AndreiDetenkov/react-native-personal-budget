b83f729850e62af2e389c70068909a47494a1361
AndreiDetenkov <a.detenkov@gmail.com>
2022-12-09 17:13:05 +0600

    feat: clear modal screen

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,35 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import React from 'react'
+import { Text } from '../components/Themed';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Container } from '../ui/styles'
 
 export default function ModalScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </View>
+    <Container>
+      <SafeAreaView>
+        <Text>Modal</Text>
+      </SafeAreaView>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+
