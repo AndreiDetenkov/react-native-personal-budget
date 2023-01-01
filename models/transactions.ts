92e146b3bc1838993f0defa0796d9b15a223afc4
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-01-02 00:20:17 +0600

    feat: remove limit

--- models/transactions.ts
+++ models/transactions.ts
@@ -7,7 +7,6 @@ export async function getTransactions() {
     .from('transactions')
     .select(`id,name,value,created_at, categories(title,id,icon)`)
     .order('created_at', { ascending: false })
-    .limit(20)
 }
 
 export async function getTransactionsByRange({ from, to }: TransactionByRangePayload) {
